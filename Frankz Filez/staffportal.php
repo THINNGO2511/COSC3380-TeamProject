bf3fd53bca27c5f55fcdb1e878148b736ce6f807
rex-v <124318537+rex-v@users.noreply.github.com>
2023-03-25 14:46:18 -0500

    Add files via upload

---
+++ Frankz Filez/staffportal.php
@@ -0,0 +1,31 @@
+<html>
+<head>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<div class="title;">
+<h1> Umaporium Staff - Web Portal </h1>
+</div>
+
+<br>
+
+<div class="column3">
+<form action="itemedit.php">
+	<button style="height:10%;width:100%"> Add Item </button>
+</form>
+</div>
+
+<div class="column3">
+<form action="inventory.php">
+	<button style="height:10%;width:100%"> View Inventory </button>
+</form>
+</div>
+
+<div class="column3">
+<form action="itemedit.php">
+	<button style="height:10%;width:100%"> Edit Item </button>
+</form>
+</div>
+</body>
+</html>
