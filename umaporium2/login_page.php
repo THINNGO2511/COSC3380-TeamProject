3d9bd51774cba1b2ac375e0b27e115db5f6d7036
sakeyna <95558092+nenvy@users.noreply.github.com>
2023-03-27 22:38:00 -0500

    Update login_page.php

--- umaporium2/login_page.php
+++ umaporium2/login_page.php
@@ -7,16 +7,11 @@
   <body>
   <header>
       <nav>
-        <ul>
-        <li><a href="index.php">Home</a></li>
-      <li><a href="about.html">About</a></li>
-      <li><a href="listings.html">Listings</a></li>
-      <li><a href="cart.html">Cart</a></li>
-        </ul>
+      <?php include('./includes/navbar.php');?>
       </nav>
     </header>
+    
     <?php
-    // for testing: redirects to "hello world" page when user is in session
     if(isset($_SESSION["userid"])) {
       header("location: index.php");
     }
