357366318a778db659fc917defe48e9874d36e29
sakeyna <95558092+nenvy@users.noreply.github.com>
2023-03-28 22:39:32 -0500

    Update index.php

--- umaporium2/index.php
+++ umaporium2/index.php
@@ -53,6 +53,7 @@ if (!isset($_SESSION["userid"])) {
 <div class="column3">
 <h2> View our latest items! </h2>
 <button class="glow-on-hover" type="button" onclick="window.location.href='listings.php'">Click for more!</button>
+<br><br><br>
 <?php
 $TestObj2 = new Dbh();
 $TestObj2->displaylist();
@@ -62,16 +63,17 @@ $TestObj2->displaylist();
 <div class="column3">
 <h2> Continue Shopping </h2>
 <button class="glow-on-hover" type="button" onclick="window.location.href='cart.php'">Go to Cart!</button>
+<br><br>
 <img src="cart.png" alt="Icon of a shopping cart." style="margin-left:18%;width:60%;height:40%;">
 </div>
 
 <div class="column3">
 <h2> Need to change something? Click below to view your account.</h2>
 <button class="glow-on-hover" type="button" onclick="window.location.href='account.php'">View my Account!</button>
-<br>
+<br><br><br>
 </div>
 
-<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
 
 <h2> Click one of the options above, or use our Navigation Bar up top for more! </h2>
 </main>
