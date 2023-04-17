a6b73268bcd4cfc7a8b1221e7204d63cae2b457c
Jacob-bach <124472415+Jacob-bach@users.noreply.github.com>
2023-04-16 19:03:47 -0500

    Add files via upload

--- umaporium2/cart.php
+++ umaporium2/cart.php
@@ -3,6 +3,18 @@ session_start();
 require_once(__DIR__ . '/connect.php');
 $dbh = new Dbh();
 $itemCount = $dbh->cartCount($_SESSION["userid"]);
+if(!empty($_POST['97'])){
+	echo 'PLEASE FOR GODS SAKE PLEASE WORK FOR ONCE';
+	$_POST['97'];
+}
+
+if(isset($_POST['submit'])){
+	$cart = $_SESSION['cartArray'];
+
+	$dbh->addQuantity($cart);
+	header("Location: order.php");
+  	exit();
+	}
 ?>
 
 <html>
@@ -26,19 +38,8 @@ $itemCount = $dbh->cartCount($_SESSION["userid"]);
 				<?php
                 if($itemCount > 0) {
                     $dbh->displayCart($_SESSION["userid"]); ?> 
-				<hr>
-				<p>Sub-total <span class = "price" style ="color:black"><b>$ <?php echo number_format($dbh->cartTotal($_SESSION["userid"]), 2, '.', '') ?></b></span></p>
-				<?php 
-				$pre = $dbh->cartTotal($_SESSION["userid"]);
-				$tax = $pre * .0725;	
-				?>
-				<p>Tax <span class = "price" style ="color:black"><b>$ <?php echo number_format($tax, 2, '.', '') ?></b>
-				<p>Total <span class = "price" style ="color:black"><b>$ <?php $total = $pre + $tax; echo number_format($total, 2, '.', '') ?></b>
-
-				
                 </div>
-				
-                <button class="btn" type="button" onclick="window.location.href='order.php'">Checkout</button>
+			
                 <br><br><br>
                 <?php
                 if(isset($_POST["empty-cart"])) {
@@ -48,7 +49,7 @@ $itemCount = $dbh->cartCount($_SESSION["userid"]);
                 <div style="text-align:left">
                 <form method="post">
                 <input type="submit" value="Empty Cart" name ="empty-cart"> 
-                </form>
+                </form>    
                 </div>
                 <?php }
                 else { ?> 
@@ -58,5 +59,6 @@ $itemCount = $dbh->cartCount($_SESSION["userid"]);
 			</div>
 		</div>
 	</body>
+	
 			
 </html>
